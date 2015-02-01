ccaff363d3a70163394a729bb2b8dcbf73c59a90
Thomas Kluyver <takowl@gmail.com>
2015-02-01 13:58:05 -0800

    Initial commit

---
+++ testpath/asserts.py
@@ -0,0 +1,80 @@
+import os
+import stat
+
+def _stat_for_assert(path, follow_symlinks=True, msg=None):
+    stat = os.stat if follow_symlinks else os.lstat
+    try:
+        return stat(path)
+    except OSError:
+        if msg is None:
+            msg = "Path does not exist, or can't be stat-ed: %r" % path
+        raise AssertionError(msg)
+
+def assert_path_exists(path, msg=None):
+    """Assert that something exists at the given path.
+    """
+    _stat_for_assert(path, True, msg)
+
+def assert_not_path_exists(path, msg=None):
+    """Assert that nothing exists at the given path.
+    """
+    if os.path.exists(path):
+        if msg is None:
+            msg = "Path exists: %r" % path
+        raise AssertionError(msg)
+
+def assert_isfile(path, follow_symlinks=True, msg=None):
+    """Assert that path exists and is a regular file.
+    
+    With follow_symlinks=True, the default, this will pass if path is a symlink
+    to a regular file. With follow_symlinks=False, it will fail in that case.
+    """
+    st = _stat_for_assert(path, follow_symlinks, msg)
+    if not stat.S_ISREG(st.st_mode):
+        if msg is None:
+            msg = "Path exists, but is not a regular file: %r" % path
+        raise AssertionError(msg)
+
+def assert_isdir(path, follow_symlinks=True, msg=None):
+    """Assert that path exists and is a directory.
+    
+    With follow_symlinks=True, the default, this will pass if path is a symlink
+    to a directory. With follow_symlinks=False, it will fail in that case.
+    """
+    st = _stat_for_assert(path, follow_symlinks, msg)
+    if not stat.S_ISDIR(st.st_mode):
+        if msg is None:
+            msg = "Path exists, but is not a directory: %r" % path
+        raise AssertionError(msg)
+
+
+_link_target_msg = """Symlink target of:
+  {path}
+Expected:
+  {expected}
+Actual:
+  {actual}
+"""
+
+def assert_islink(path, to=None, msg=None):
+    """Assert that path exists and is a symlink.
+    
+    If to is specified, also check that it is the target of the symlink.
+    """
+    st = _stat_for_assert(path, False, msg)
+    if not stat.S_ISLNK(st.st_mode):
+        if msg is None:
+            msg = "Path exists, but is not a symlink: %r" % path
+        raise AssertionError(msg)
+    
+    if to is not None:
+        target = os.readlink(path)
+        # TODO: Normalise the target to an absolute path?
+        if target != to:
+            if msg is None:
+                msg = _link_target_msg.format(path=path, expected=to, actual=target)
+            raise AssertionError(msg)
+
+# TODO: assert_not_isfile, assert_not_isdir, assert_not_islink - what do they
+# do when the path doesn't exist. Pass, for consistency with 'not isfile()'
+# etc., or fail because there's already assert_not_path_exists?
---
+++ tests/test_asserts.py
@@ -0,0 +1,62 @@
+import os
+import unittest
+
+from testpath.asserts import *
+from testpath.tempdir import TemporaryDirectory
+
+class TestAssertFunctions(unittest.TestCase):
+    def setUp(self):
+        self.td = TemporaryDirectory()
+        self.addCleanup(self.td.cleanup)
+        
+        self.file_path = os.path.join(self.td.name, 'afile')
+        with open(self.file_path, 'w') as f:
+            f.write('Blah')
+        
+        self.dir_path = os.path.join(self.td.name, 'adir')
+        os.mkdir(self.dir_path)
+        
+        self.link_path = os.path.join(self.td.name, 'alink')
+        if os.name == 'posix':
+            # Symlinks are rarely usable on Windows, because a special
+            # permission is needed to create them.
+            os.symlink(self.file_path, self.link_path)
+        
+        self.nonexistant_path = os.path.join(self.td.name, 'doesntexist')
+    
+    def test_exists(self):
+        assert_path_exists(self.file_path)
+        assert_not_path_exists(self.nonexistant_path)
+        
+        with self.assertRaises(AssertionError):
+            assert_path_exists(self.nonexistant_path)
+        
+        with self.assertRaises(AssertionError):
+            assert_not_path_exists(self.file_path)
+    
+    def test_isfile(self):
+        assert_isfile(self.file_path)
+        assert_isfile(self.link_path)  # Follows the link by default
+        
+        with self.assertRaises(AssertionError):
+            assert_isfile(self.dir_path)
+        
+        with self.assertRaises(AssertionError):
+            assert_isfile(self.link_path, follow_symlinks=False)
+
+    def test_isdir(self):
+        assert_isdir(self.dir_path)
+        
+        with self.assertRaises(AssertionError):
+            assert_isdir(self.file_path)
+
+    def test_islink(self):
+        assert_islink(self.link_path, to=self.file_path)
+        
+        with self.assertRaises(AssertionError) as c:
+            assert_islink(self.file_path)
+        self.assertIn('not a symlink', str(c.exception))
+        
+        with self.assertRaises(AssertionError) as c:
+            assert_islink(self.link_path, to=self.dir_path)
+        self.assertIn('target of', str(c.exception))
