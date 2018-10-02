f2323657a5ee4dd397f1088b4d4603f0b5a85ffd
Thomas Kluyver <thomas@kluyver.me.uk>
2018-10-02 08:31:42 +0200

    Version number -> 0.4.2

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,7 @@ copyright = '2015, Jupyter Development Team'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4.1'
+version = '0.4.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
